04d844e320a18feb7d0805fa71efbac07e514fbe
Ankit Kirtane <159574007+Tikna09@users.noreply.github.com>
2026-04-22 16:12:49 +0530

    Update geo_map.py

--- Week 4/geo_map.py
+++ Week 4/geo_map.py
@@ -1,1 +1,17 @@
+import requests
+import folium
 
+ip = "8.8.8.8"
+
+response = requests.get(f"http://ip-api.com/json/{ip}")
+data = response.json()
+
+lat=data["lat"]
+lon=data["lon"]
+country=data["country"]
+
+m = folium.Map(loction=[lat, lon], zoom_start=5)
+folium.Marker([lat, lon], popup=f"Attacker IP: {ip} | Country:{country} ").add_to(m)
+m.save("attack_map.html")
+
+print("Map   saved as attack_map.html")
